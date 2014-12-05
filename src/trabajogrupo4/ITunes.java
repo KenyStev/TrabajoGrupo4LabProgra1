b7441ab22a1a509f712c36fca6b408de0818ed56
KenyStev <kjestevez.0795@gmail.com>
2014-12-05 07:22:47 -0600

    clase ITunes creada

--- src/trabajogrupo4/ITunes.java
+++ src/trabajogrupo4/ITunes.java
@@ -9,13 +9,6 @@ package trabajogrupo4;
  *
  * @author zerokull
  */
-public class TrabajoGrupo4 {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+public class ITunes {
     
 }
